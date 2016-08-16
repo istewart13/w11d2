cba4db57aa71c6dd349486d752fd3b3428f7d3e3
Iain Stewart <iain.stewart08@gmail.com>
2016-08-16 14:26:23 +0100

    initial commit

---
+++ app/src/main/java/com/example/user/magicanimals/MagicAnimals.java
@@ -0,0 +1,15 @@
+package com.example.user.magicanimals;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 16/08/2016.
+ */
+public class MagicAnimals extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
